fea6b7f29786e169440c308e6ae4022f3eda2eac
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2018-11-19 16:52:09 +0000

    fix path of mysql binary

--- manifests/dbcreate.pp
+++ manifests/dbcreate.pp
@@ -1,12 +1,12 @@
 
-define centreon::dbcreate( $user, $password, $sql ) {
+define centreon::dbcreate( $mysql_root_password = 'changeme', $user, $password, $sql ) {
   $ml_exec = @(EOT)
-    /usr/bin/mysql -uroot -p$mysql_password -e \
+    /bin/mysql -uroot -p$mysql_root_password -e \
     \"create database ${name}; grant all on ${name}.* to ${user}@localhost identified by '$password';\"
     /usr/bin/mysql -u${user} -p${password} ${name} <$sql
   | EOT
   exec { "create-${name}-db":
-    unless  => "/usr/bin/mysql -u${user} -p${password} ${name}",
+    unless  => "/bin/mysql -u${user} -p${password} ${name}",
     command => $ml_exec,
     require => Service['mysqld'],
   }
