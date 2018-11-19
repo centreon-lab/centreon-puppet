baf75065cdf2cda434f8a7e99e40c5e6f84c148a
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2018-11-19 16:53:44 +0000

    fix path

--- manifests/dbcreate.pp
+++ manifests/dbcreate.pp
@@ -3,7 +3,7 @@ define centreon::dbcreate( $mysql_root_password = 'changeme', $user, $password, 
   $ml_exec = @(EOT)
     /bin/mysql -uroot -p$mysql_root_password -e \
     \"create database ${name}; grant all on ${name}.* to ${user}@localhost identified by '$password';\"
-    /usr/bin/mysql -u${user} -p${password} ${name} <$sql
+    /bin/mysql -u${user} -p${password} ${name} <$sql
   | EOT
   exec { "create-${name}-db":
     unless  => "/bin/mysql -u${user} -p${password} ${name}",
