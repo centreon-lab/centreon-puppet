38e0f3461236c26bedc57acfb38c7ba5c4d02839
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2018-11-19 17:16:27 +0000

    remove fixed value of value

--- manifests/dbcreate.pp
+++ manifests/dbcreate.pp
@@ -1,5 +1,5 @@
 
-define centreon::dbcreate( $mysql_root_password = 'changeme', $user, $password, $sql ) {
+define centreon::dbcreate( $user, $password, $sql ) {
   exec { "create-${name}-db":
     unless  => "/bin/mysql -u${user} -p${password} ${name}",
     command => "/bin/mysql -uroot -p$mysql_root_password -e \"create database ${name}; grant all on ${name}.* to ${user}@localhost identified by '${password}';\" && /bin/mysql -u${user} -p${password} ${name} < ${sql}",
