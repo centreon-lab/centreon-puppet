15feaa60c2c6c0dd060eda6d5682f7187f9c5988
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2018-11-19 17:11:57 +0000

    remove multiline in exec

--- manifests/dbcreate.pp
+++ manifests/dbcreate.pp
@@ -1,13 +1,8 @@
 
 define centreon::dbcreate( $mysql_root_password = 'changeme', $user, $password, $sql ) {
-  $ml_exec = @(EOT)
-    /bin/mysql -uroot -p$mysql_root_password -e \
-    \"create database ${name}; grant all on ${name}.* to ${user}@localhost identified by '$password';\"
-    /bin/mysql -u${user} -p${password} ${name} <$sql
-  | EOT
   exec { "create-${name}-db":
     unless  => "/bin/mysql -u${user} -p${password} ${name}",
-    command => $ml_exec,
+    command => "/bin/mysql -uroot -p$mysql_root_password -e \"create database ${name}; grant all on ${name}.* to ${user}@localhost identified by '${password}';\" && /bin/mysql -u${user} -p${password} ${name} < ${sql}",
     require => Service['mysqld'],
   }
 }
