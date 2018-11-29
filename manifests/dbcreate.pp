e73bcae73c730df339affdf1a423319023240736
Luiz G. Costa <luizgustavo@luizgustavo.pro.br>
2018-11-29 14:44:01 +0000

    remove require

--- manifests/dbcreate.pp
+++ manifests/dbcreate.pp
@@ -4,7 +4,6 @@ define centreon::dbcreate( $user, $password, $sql ) {
     unless  => "/bin/mysql -u${user} -p${password} ${name}",
     command => "/bin/mysql -uroot -p'$mysql_root_password' -e \"CREATE DATABASE IF NOT EXISTS ${name}; GRANT ALL ON ${name}.* TO ${user}@localhost identified by '${password}';\" && /bin/mysql -u${user} -p'${password}' ${name} < ${sql}",
     require => [
-      File['/usr/share/centreon/install'],
       Package[$centreon_web_packages],
       Service['mysqld'],
       Exec['Run deploy initial configuration']
